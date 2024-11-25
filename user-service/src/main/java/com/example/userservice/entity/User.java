6590c7267f52beede78b0c08320514ccfc86c20b
Dias Rakhman <rahmanovdias13@gmail.com>
2024-11-25 11:20:36 +0500

    first commit

---
+++ user-service/src/main/java/com/example/userservice/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.userservice.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+    private String phone;
+}
